b78a7fb5aea470ed99cb135e8b304924f21b73e8
Pierre Alexandre Tremblay <info@pierrealexandretremblay.com>
2018-04-24 16:45:26 +0100

    now with discreet start and stop points under the method 'chunkSwap'

--- BufRev.cpp
+++ BufRev.cpp
@@ -16,13 +16,10 @@ void BufRev(World *world, struct SndBuf *buf, struct sc_msg_iter *msg)
   float temp;
   int loadd,hiadd;
 
-  // Print("size = %d\nchans = %d\nframes = %d\nhalfframe = %d\n",size,chans,frames,halfframe);
-
   for (int i = 0; i < halfframe; ++i) {
     for (int j = 0; j < chans; ++j) {
       hiadd = j+size-((i+1)*chans);
       loadd = j+(i*chans);
-      // Print("%d %d %d %d\n", i, j, hiadd, loadd);
       temp = data[hiadd];
       data[hiadd] = data[loadd];
       data[loadd] = temp;
@@ -30,7 +27,68 @@ void BufRev(World *world, struct SndBuf *buf, struct sc_msg_iter *msg)
   }
 }
 
+void chunkSwap(World *world, struct SndBuf *buf, struct sc_msg_iter *msg)
+{
+	float *data = buf->data;
+  // int size = buf->samples;
+  int chans = buf->channels;
+  int frames = buf->frames;
+
+  // reading the arguments with default values
+  int srcStartAt = msg->geti(0);
+  int dstStartAt = msg->geti(-1);
+  int numFrames = msg->geti(0);
+
+  // should default value happen, retrieve them
+  if (dstStartAt == -1)
+    dstStartAt = frames - 1; // defaults to last sample
+  if (numFrames == 0)
+    numFrames = (frames * -1) / 2;
+
+  // define end writing frames
+  int AbsNumFrames = std::abs(numFrames);
+  int srcStopAt = srcStartAt + AbsNumFrames;
+  int dstStopAt = dstStartAt + numFrames;
+  // Print("size = %d\nchans = %d\nframes = %d\nnumFrames = %d\nAbsNumFrames = %d\n",size,chans,frames,numFrames,AbsNumFrames);
+  // Print("srcStartAt = %d\nsrcStopAt = %d\ndstStartAt = %d\ndstStopAt = %d\n",srcStartAt,srcStopAt,dstStartAt,dstStopAt);
+  // check the boundaries
+  if (srcStartAt < 0 || srcStartAt >= frames) {
+    Print("chunkSwap is not happy because the source starting is outside the buffer.\n");
+    return;
+  }
+  else if (srcStopAt < 0 || srcStopAt >= frames) {
+    Print("chunkSwap is not happy because the source finishing is outside the buffer.\n");
+    return;
+  }
+  else if (dstStartAt < 0 || dstStartAt >= frames) {
+    Print("chunkSwap is not happy because the destination starting is outside the buffer.\n");
+    return;
+  }
+  else if (dstStopAt < 0 || dstStopAt >= frames) {
+    Print("chunkSwap is not happy because the destination finishing is outside the buffer.\n");
+    return;
+  }
+
+  int dstStep = numFrames == AbsNumFrames ? 1 : -1;
+  float temp;
+  int srcAdd,dstAdd;
+
+  for (int i = srcStartAt; i < srcStopAt; ++i) {
+    for (int j = 0; j < chans; ++j) {
+      dstAdd = j+(dstStartAt*chans);
+      srcAdd = j+(i*chans);
+      // Print("%d %d %d %d\n", i, j, hiadd, loadd);
+      temp = data[dstAdd];
+      data[dstAdd] = data[srcAdd];
+      data[srcAdd] = temp;
+    }
+    dstStartAt+= dstStep;
+  }
+}
+
+
 PluginLoad(BufRevUGens) {
   ft = inTable;
   DefineBufGen("reverse", BufRev);
+  DefineBufGen("chunkSwap", chunkSwap);
 }
--- BufRev.sc
+++ BufRev.sc
@@ -1,7 +1,11 @@
-// adds an instance method 'reverse' to the Buffer class
+// adds 2 instance methods to the Buffer class: 'reverse' and 'chunkSwap'
 + Buffer {
 	reverse {
 		if(bufnum.isNil) { Error("Cannot call % on a % that has been freed".format(thisMethod.name, this.class.name)).throw };
 		server.listSendMsg([\b_gen, bufnum, "reverse"])
+	}
+	chunkSwap { arg srcStartAt = 0, dstStartAt = -1, numFrames = 0;
+		if(bufnum.isNil) { Error("Cannot call % on a % that has been freed".format(thisMethod.name, this.class.name)).throw };
+		server.listSendMsg([\b_gen, bufnum, "chunkSwap", srcStartAt, dstStartAt, numFrames])
 	}
 }
